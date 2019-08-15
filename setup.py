c5f30c791feb48fc0dc4032e66cdcc1ed15beec6
Kefirock <id.justevolution@gmail.com>
2019-08-15 14:46:12 +0000

    Version fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="testtools1",
-    version="1.0.0",
+    version="1.0.1",
     author="TakeWing",
     author_email="n.ostroukhov@takewing.ru",
     description="EthTools framework",
